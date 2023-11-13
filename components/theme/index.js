09f56cc89d3b8146ea29b0737aefdc6dc57c9818
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-11-13 19:49:00 +0000

    started nativation

---
+++ components/theme/index.js
@@ -0,0 +1,10 @@
+const colors = {
+    primary: '#f4f3f5',
+    primaryBlue: '#2ea6fd',
+    primaryShadow: '#cde4f6',
+    lightPink: '#ffe6db',
+    lightMolve: '#e1e1fe',
+    green: '#58dc18',
+    red: '#f95555',
+    pail: "#a2ffdb"
+}
---
+++ screens/MainNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const MainNav = () => {
+    return (
+        <View>
+            <Text>
+                MainNav
+            </Text>
+        </View>
+    )
+}
